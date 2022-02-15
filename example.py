6ccfe1176a09f25bc976385f2ac24ad9efa59994
seaneriksson <46273556+seaneriksson@users.noreply.github.com>
2022-02-15 11:43:41 -0500

    Add files via upload

--- example.py
+++ example.py
@@ -8,27 +8,25 @@ sense.clear
 
 timeStamp= datetime.now()
 
+##create temperature data list
 temperature_data = [ ]
 
 
 counter = 0
-while counter < 1800:
+while counter < 1800: ## run for 30 minutes
     temperature_data.append(timeStamp)
     temp = sense.get_temperature()
     temperature_data.append(temp)
-    sense.show_message(str(round(temp)) + "C")
+    sense.show_message(str(round(temp, 1)) + "C")
     print(timeStamp)
     print(temperature_data)
     sleep(1)  ## run every 1 minute
     counter = counter + 1
-    
-    
-    
 
 with open('data.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(temperature_data)
 
-	#data_writer.writerow(str(dateTime))
+
 
 
