e109db484e9f6e139c5765681a3943fcf07e227c
seaneriksson <46273556+seaneriksson@users.noreply.github.com>
2022-02-17 11:29:46 -0500

    Add files via upload

--- example.py
+++ example.py
@@ -6,26 +6,30 @@ from time import time, sleep
 sense = SenseHat()
 sense.clear
 
-timeStamp= datetime.now()
+
+##create time data list
+time_data = []
 
 ##create temperature data list
-temperature_data = [ ]
+temp_data = []
 
 
 counter = 0
-while counter < 1800: ## run for 30 minutes
-    temperature_data.append(timeStamp)
-    temp = sense.get_temperature()
-    temperature_data.append(temp)
-    sense.show_message(str(round(temp, 1)) + "C")
+while counter < 60: ## run for 1 minute
+    timeStamp= datetime.now()
+    time_data.append(timeStamp)
+    tempValue = sense.get_temperature()
+    temp_data.append(tempValue)
+    sense.show_message(str(round(tempValue, 1)) + "C")
     print(timeStamp)
-    print(temperature_data)
+    print(temp_data)
     sleep(1)  ## run every 1 minute
     counter = counter + 1
 
 with open('data.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(temperature_data)
+    for w in range(len(time_data)):
+        writer.writerow([time_data[w], temp_data[w]])
 
 
 
